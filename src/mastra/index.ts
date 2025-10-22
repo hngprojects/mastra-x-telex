20169a93e36f99c3feae17ef63befab597fa85d7
phoenix <ayomikuntemitope.araoye@gmail.com>
2025-10-22 11:14:19 -0700

    feat: agent

---
+++ src/mastra/index.ts
@@ -0,0 +1,23 @@
+
+import { Mastra } from '@mastra/core/mastra';
+import { PinoLogger } from '@mastra/loggers';
+import { LibSQLStore } from '@mastra/libsql';
+import { weatherWorkflow } from './workflows/weather-workflow';
+import { weatherAgent } from './agents/weather-agent';
+import { toolCallAppropriatenessScorer, completenessScorer, translationScorer } from './scorers/weather-scorer';
+
+export const mastra = new Mastra({
+  workflows: { weatherWorkflow },
+  agents: { weatherAgent },
+  scorers: { toolCallAppropriatenessScorer, completenessScorer, translationScorer },
+  storage: new LibSQLStore({
+    url: ":memory:",
+  }),
+  logger: new PinoLogger({
+    name: 'Mastra',
+    level: 'debug',
+  }),
+  observability: {
+    default: { enabled: true },
+  },
+});
